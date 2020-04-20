cff932c81637fb37315de7a24da0db578ab2ad56
chylli-binary <chylli@binary.com>
2020-04-20 14:31:49 +0800

    emit event instead of log

--- lib/Mojo/Server/Daemon.pm
+++ lib/Mojo/Server/Daemon.pm
@@ -110,7 +110,7 @@ sub _build_tx {
       my $c = $self->{connections}{$id};
 
       unless ($c) {
-          $self->app->log->warn("There is no connection $id now. Maybe disconnected by the pair because this transaction took too long");
+          $self->emit(client_disconnect => $id);
           return;
       }
 
